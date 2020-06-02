6199eda58e3093c6e1e3780462fc67185c16bfa5
yclim95 <lyc950421@gmail.com>
2020-06-02 18:49:35 +0800

    Update .java file

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -3,6 +3,9 @@ CREATED BY: YC Lim
  */
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        System.out.print("dasaa"); // Print in the same line
+        System.out.println("Hello Word"); // print text & a new line
+        System.out.println ("dkadlakdjlajdaj/" +
+                "dasdadad"); // Print multiple line
     }
 }

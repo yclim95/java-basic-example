eb928a787579178ec6d52bffb959e8cce5ea5d2b
yclim95 <lyc950421@gmail.com>
2020-06-02 19:11:21 +0800

    Update Image & .java File

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # README
-1. ![HELLOWORLD.java](https://github.com/yclim95/java-basic-example/blob/master/img/helloworld.PNG)
+1. HelloWord.java ![HELLOWORLD.java](https://github.com/yclim95/java-basic-example/blob/master/img/helloworld.PNG)
--- src/HelloWord.java
+++ src/HelloWord.java
@@ -3,9 +3,9 @@ CREATED BY: YC Lim
  */
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.print("dasaa"); // Print in the same line
-        System.out.println("Hello Word"); // print text & a new line
-        System.out.println ("dkadlakdjlajdaj/" +
-                "dasdadad"); // Print multiple line
+        System.out.print("Print"); // Print in the same line
+        System.out.println("Print Line"); // print text & a new line
+        System.out.println ("Print Multiple/" +
+                "Line"); // Print multiple line
     }
 }

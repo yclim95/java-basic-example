ddbb94d3a2a5414bb01aaf10f3c8709a78027332
yclim95 <lyc950421@gmail.com>
2020-06-02 19:40:20 +0800

    Added image

--- src/myStaticMethod.java
+++ src/myStaticMethod.java
@@ -4,4 +4,11 @@ CREATED BY: YC Lim
 public class myStaticMethod {
     // Static Methods - a named group of statements
     // Called by main
+    public static void main(String[] args) {
+        myFirstMethod();
+    }
+
+    public static void myFirstMethod(){
+        System.out.println("Hello Word");
+    }
 }
